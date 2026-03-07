0b4b5ab412c2b9f031fe610a8050fb27bd07de58
Yi Zhong <tohyzhong@gmail.com>
2026-03-07 15:11:03 +0800

    Add unsubmitted Assignment exception

---
+++ src/main/java/cpp/model/assignment/exceptions/AssignmentNotSubmittedException.java
@@ -0,0 +1,7 @@
+package cpp.model.assignment.exceptions;
+
+public class AssignmentNotSubmittedException extends RuntimeException {
+    public AssignmentNotSubmittedException(String message) {
+        super(message);
+    }
+}
