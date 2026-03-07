c4f96a23bffd72c043a90c45ccd8148df9275d90
Yi Zhong <tohyzhong@gmail.com>
2026-03-08 00:15:57 +0800

    Add unit tests for Assignment

---
+++ src/test/java/cpp/model/assignment/AssignmentTest.java
@@ -0,0 +1,44 @@
+package cpp.model.assignment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import cpp.testutil.AssignmentBuilder;
+
+public class AssignmentTest {
+
+    @Test
+    public void equals_sameIdNameDeadline_returnsTrue() {
+        Assignment a1 = new AssignmentBuilder().withId("id1").withName("Assignment X").withDeadline("13-12-2020 10:00")
+                .build();
+        Assignment a2 = new AssignmentBuilder().withId("id1").withName("Assignment X").withDeadline("13-12-2020 10:00")
+                .build();
+        Assertions.assertEquals(a1, a2);
+    }
+
+    @Test
+    public void equals_differentId_returnsFalse() {
+        Assignment a1 = new AssignmentBuilder().withId("id1").build();
+        Assignment a2 = new AssignmentBuilder().withId("id2").build();
+        Assertions.assertNotEquals(a1, a2);
+    }
+
+    @Test
+    public void hashCode_sameNameDeadline_equal() {
+        Assignment a1 = new AssignmentBuilder().withId("id1").withName("Same").withDeadline("13-12-2020 10:00")
+                .build();
+        Assignment a2 = new AssignmentBuilder().withId("id2").withName("Same").withDeadline("13-12-2020 10:00")
+                .build();
+        Assertions.assertEquals(a1.hashCode(), a2.hashCode());
+    }
+
+    @Test
+    public void toString_containsNameAndDeadline() {
+        Assignment a = new AssignmentBuilder().withId("id1").withName("ToStringTest").withDeadline("13-12-2020 10:00")
+                .build();
+        String s = a.toString();
+        Assertions.assertTrue(s.contains(a.getName().fullName));
+        Assertions.assertTrue(s.contains(a.getDeadline().toString()));
+    }
+
+}
