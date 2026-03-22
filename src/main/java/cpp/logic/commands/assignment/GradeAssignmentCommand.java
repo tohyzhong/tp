ed339581b37843e2ff2b7ee59719ab90e18fa1fe
Yi Zhong <tohyzhong@gmail.com>
2026-03-22 23:42:55 +0800

    Add grade assignment command

--- src/main/java/cpp/logic/commands/assignment/GradeAssignmentCommand.java
+++ src/main/java/cpp/logic/commands/assignment/GradeAssignmentCommand.java
@@ -9,13 +9,21 @@ import java.util.Set;
 
 import cpp.commons.core.index.Index;
 import cpp.commons.util.ToStringBuilder;
+import cpp.logic.Messages;
 import cpp.logic.commands.Command;
 import cpp.logic.commands.CommandResult;
+import cpp.logic.commands.CommandUtil;
 import cpp.logic.commands.exceptions.CommandException;
 import cpp.logic.parser.CliSyntax;
+import cpp.logic.parser.ParserUtil;
 import cpp.model.Model;
+import cpp.model.assignment.Assignment;
 import cpp.model.assignment.AssignmentName;
+import cpp.model.assignment.exceptions.ContactAssignmentAlreadyGradedException;
+import cpp.model.assignment.exceptions.ContactAssignmentNotFoundException;
+import cpp.model.assignment.exceptions.ContactAssignmentNotSubmittedException;
 import cpp.model.contact.Contact;
+import cpp.model.util.AssignmentUtil;
 
 /**
  * Grades an assignment for a contact. The assignment must have been submitted
@@ -41,36 +49,77 @@ public class GradeAssignmentCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = """
             Graded assignment: %1$s on %2$s for %3$s contact(s) with score %4$.2f.
-            Contacts graded: %5$s""";
+            Contacts graded: %5$s
+            Contacts not graded (already graded): %6$s
+            Contacts not graded (not submitted yet): %7$s
+            Contacts not graded (not allocated the assignment): %8$s""";
 
     private final AssignmentName assignmentName;
     private final List<Index> contactIndices;
     private final Set<Contact> contactsToGrade = new HashSet<>();
     private final float score;
+    private final LocalDateTime gradingDate;
 
     private int gradedCount = 0;
     private int alreadyGradedCount = 0;
-    private int notSubmittedOrNotAllocatedCount = 0;
+    private int notSubmittedCount = 0;
+    private int notAllocatedCount = 0;
     private StringBuilder gradedContacts = new StringBuilder();
     private StringBuilder alreadyGradedContacts = new StringBuilder();
-    private StringBuilder notSubmittedOrNotAllocatedContacts = new StringBuilder();
+    private StringBuilder notSubmittedContacts = new StringBuilder();
+    private StringBuilder notAllocatedContacts = new StringBuilder();
 
     /**
      * Creates a GradeAssignmentCommand to grade the specified assignment for the
      * specified contacts with the given grade info.
      */
-    public GradeAssignmentCommand(AssignmentName assignmentName, List<Index> contactIndices, float score) {
+    public GradeAssignmentCommand(AssignmentName assignmentName, List<Index> contactIndices, float score,
+            LocalDateTime gradingDate) {
         this.assignmentName = assignmentName;
         this.contactIndices = new ArrayList<>(contactIndices);
         this.score = score;
+        this.gradingDate = gradingDate;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         Objects.requireNonNull(model);
 
-        return new CommandResult(String.format(GradeAssignmentCommand.MESSAGE_SUCCESS, this.assignmentName,
-                LocalDateTime.now(), this.contactsToGrade.size(), this.score, this.gradedContacts.toString()));
+        List<Assignment> assignmentList = model.getAddressBook().getAssignmentList();
+
+        Assignment assignmentToGrade = AssignmentUtil.findAssignment(assignmentList, this.assignmentName);
+
+        if (assignmentToGrade == null) {
+            throw new CommandException(Messages.MESSAGE_ASSIGNMENT_NOT_FOUND);
+        }
+
+        List<Contact> lastShownContactList = model.getFilteredContactList();
+
+        CommandUtil.checkContactIndices(lastShownContactList, this.contactIndices);
+
+        this.gradeByContactIndices(model, assignmentToGrade, lastShownContactList);
+
+        if (this.gradedCount == 0) {
+            throw new CommandException(SubmitAssignmentCommand.MESSAGE_SUBMISSION_FAILED);
+        }
+
+        if (this.alreadyGradedCount == 0) {
+            this.alreadyGradedContacts.append("None");
+        }
+
+        if (this.notSubmittedCount == 0) {
+            this.notSubmittedContacts.append("None");
+        }
+
+        if (this.notAllocatedCount == 0) {
+            this.notAllocatedContacts.append("None");
+        }
+
+        return new CommandResult(
+                String.format(GradeAssignmentCommand.MESSAGE_SUCCESS, Messages.format(assignmentToGrade),
+                        this.gradingDate.format(ParserUtil.DATETIME_FORMATTER), this.gradedCount, this.score,
+                        this.gradedContacts.toString(), this.alreadyGradedContacts.toString(),
+                        this.notSubmittedContacts.toString(), this.notAllocatedContacts.toString()));
     }
 
     @Override
@@ -94,5 +143,72 @@ public class GradeAssignmentCommand extends Command {
                 .add("contactIndices", this.contactIndices)
                 .add("score", this.score)
                 .toString();
+    }
+
+    private void gradeByContactIndices(Model model, Assignment assignmentToUnallocate,
+            List<Contact> lastShownContactList) {
+
+        for (Index idx : this.contactIndices) {
+            Contact contact = lastShownContactList.get(idx.getZeroBased());
+
+            this.gradeByContact(model, assignmentToUnallocate, contact);
+        }
+    }
+
+    private void gradeByContact(Model model, Assignment assignment, Contact contact) {
+        if (this.contactsToGrade.contains(contact)) {
+            // Skip contacts that have already been marked as submitted through
+            // contact indices in the same command
+            return;
+        }
+
+        try {
+            model.grade(assignment, contact, this.score, this.gradingDate);
+            this.gradedCount++;
+            this.buildSuccessfulGradeString(contact.getName().fullName);
+            this.contactsToGrade.add(contact);
+
+        } catch (ContactAssignmentNotFoundException e) {
+            // Skip contacts that don't have the assignment allocated.
+            this.notAllocatedCount++;
+            this.buildNotAllocatedString(contact.getName().fullName);
+        } catch (ContactAssignmentNotSubmittedException e) {
+            // Skip contacts that have not submitted the assignment.
+            this.notSubmittedCount++;
+            this.buildNotSubmittedString(contact.getName().fullName);
+
+        } catch (ContactAssignmentAlreadyGradedException e) {
+            // Skip contacts that have already been marked as graded.
+            this.alreadyGradedCount++;
+            this.buildAlreadyGradedString(contact.getName().fullName);
+        }
+    }
+
+    private void buildSuccessfulGradeString(String contactName) {
+        if (this.gradedContacts.length() > 0) {
+            this.gradedContacts.append(", ");
+        }
+        this.gradedContacts.append(contactName);
+    }
+
+    private void buildAlreadyGradedString(String contactName) {
+        if (this.alreadyGradedContacts.length() > 0) {
+            this.alreadyGradedContacts.append(", ");
+        }
+        this.alreadyGradedContacts.append(contactName);
+    }
+
+    private void buildNotSubmittedString(String contactName) {
+        if (this.notSubmittedContacts.length() > 0) {
+            this.notSubmittedContacts.append(", ");
+        }
+        this.notSubmittedContacts.append(contactName);
+    }
+
+    private void buildNotAllocatedString(String contactName) {
+        if (this.notAllocatedContacts.length() > 0) {
+            this.notAllocatedContacts.append(", ");
+        }
+        this.notAllocatedContacts.append(contactName);
     }
 }
